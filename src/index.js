9a01438000f4bd65b14d76d63bae97854b37d56a
Frosthaven <shane.stanley1983@gmail.com>
2021-11-14 15:23:15 -0500

    split code out of index into trayApp and defaultSettings

--- src/index.js
+++ src/index.js
@@ -4,102 +4,19 @@
 
 // imports *********************************************************************
 
-// built-in
-import os from 'os';
-import path from 'path';
-
-// external
-import SysTray from 'systray2';
-
 // local
-import { PRIORITIES } from './lib/processManager';
 import { setupPersistantSystray } from './lib/persistantSysTray';
 import { startAudioSync } from './lib/audioSyncManager';
 
-// menu items
-import { itemListBindings } from './menuItems/itemListBindings';
-import { itemListRestarts } from './menuItems/itemListRestarts';
-import { itemListPatches } from './menuItems/itemListPatches';
-import { itemShowVoicemeeter } from './menuItems/voicemeeter/itemShowVoicemeeter';
-import { itemRestartVoicemeeter } from './menuItems/voicemeeter/itemRestartVoicemeeter';
-import { itemRestartAudioEngine } from './menuItems/voicemeeter/itemRestartAudioEngine';
-import { itemVisitGithub } from './menuItems/support/itemVisitGithub';
-import { itemDonate } from './menuItems/support/itemDonate';
-import { itemExit } from './menuItems/itemExit';
-import { STRING_MENU_ITEMS, STRING_METADATA } from './lib/strings';
+import { defaults } from './defaultSettings';
+import { trayApp } from './trayApp';
 
-// configuration ***************************************************************
-
-// default settings
-const defaults = {
-    polling_rate: 100,
-    gain_min: -60,
-    gain_max: 12,
-    start_with_windows: true,
-    limit_db_gain_to_0: false,
-    remember_volume: false,
-    disable_donate: false,
-    audiodg: {
-        priority: PRIORITIES.HIGH,
-        affinity: 2,
-    },
-    toggles: [
-        {
-            setting: 'restart_audio_engine_on_device_change',
-            value: false,
-        },
-        {
-            setting: 'restart_audio_engine_on_app_launch',
-            value: false,
-        },
-    ],
-};
 const settingsPath = `${__dirname}/settings.json`;
-
-// tray app menu
-const trayApp = {
-    menu: {
-        icon:
-            os.platform() === 'win32'
-                ? path.normalize(__dirname + '/assets/app.ico')
-                : path.normalize(__dirname + '/assets/app.png'),
-        title: STRING_METADATA.friendlyname,
-        tooltip: STRING_METADATA.friendlyname,
-        items: [
-            {
-                title: `${STRING_METADATA.friendlyname.toUpperCase()}\tv${
-                    STRING_METADATA.version
-                }`,
-                enabled: false,
-            },
-            itemListBindings(),
-            itemListRestarts(),
-            itemListPatches(),
-            SysTray.separator,
-            { title: STRING_MENU_ITEMS['itemVMTitle'], enabled: false },
-            itemShowVoicemeeter(),
-            itemRestartVoicemeeter(),
-            itemRestartAudioEngine(),
-            SysTray.separator,
-            { title: STRING_MENU_ITEMS['itemSupportTitle'], enabled: false },
-            itemVisitGithub(),
-            itemDonate(),
-            SysTray.separator,
-            itemExit({
-                click: () => {
-                    process.exit();
-                },
-            }),
-        ],
-    },
-    debug: false,
-    copyDir: true, // this is required since we're compiling to an exe
-};
 
 // initialize the tray app *****************************************************
 
 console.log('Voicemeeter Windows Volume started, Process ID: ', process.pid);
-const systray = setupPersistantSystray({
+setupPersistantSystray({
     trayApp,
     defaults,
     settingsPath,
---
+++ src/trayApp.js
@@ -0,0 +1,61 @@
+// built-in
+import os from 'os';
+import path from 'path';
+
+// external
+import SysTray from 'systray2';
+
+// internal
+import { STRING_MENU_ITEMS, STRING_METADATA } from './lib/strings';
+
+// menu items
+import { itemListBindings } from './menuItems/itemListBindings';
+import { itemListRestarts } from './menuItems/itemListRestarts';
+import { itemListPatches } from './menuItems/itemListPatches';
+import { itemShowVoicemeeter } from './menuItems/voicemeeter/itemShowVoicemeeter';
+import { itemRestartVoicemeeter } from './menuItems/voicemeeter/itemRestartVoicemeeter';
+import { itemRestartAudioEngine } from './menuItems/voicemeeter/itemRestartAudioEngine';
+import { itemVisitGithub } from './menuItems/support/itemVisitGithub';
+import { itemDonate } from './menuItems/support/itemDonate';
+import { itemExit } from './menuItems/itemExit';
+
+const trayApp = {
+    menu: {
+        icon:
+            os.platform() === 'win32'
+                ? path.normalize(__dirname + '/assets/app.ico')
+                : path.normalize(__dirname + '/assets/app.png'),
+        title: STRING_METADATA.friendlyname,
+        tooltip: STRING_METADATA.friendlyname,
+        items: [
+            {
+                title: `${STRING_METADATA.friendlyname.toUpperCase()}\tv${
+                    STRING_METADATA.version
+                }`,
+                enabled: false,
+            },
+            itemListBindings(),
+            itemListRestarts(),
+            itemListPatches(),
+            SysTray.separator,
+            { title: STRING_MENU_ITEMS['itemVMTitle'], enabled: false },
+            itemShowVoicemeeter(),
+            itemRestartVoicemeeter(),
+            itemRestartAudioEngine(),
+            SysTray.separator,
+            { title: STRING_MENU_ITEMS['itemSupportTitle'], enabled: false },
+            itemVisitGithub(),
+            itemDonate(),
+            SysTray.separator,
+            itemExit({
+                click: () => {
+                    process.exit();
+                },
+            }),
+        ],
+    },
+    debug: false,
+    copyDir: true, // this is required since we're compiling to an exe
+};
+
+export { trayApp };
